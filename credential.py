9cbb901f49306e7e4e208e1a3a4ce54355e81437
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-14 11:46:50 +0200

    add def for save credentials

---
+++ credential.py
@@ -0,0 +1,21 @@
+class Credential:
+    """
+    class that generates accountName,username and password of credentials 
+    """
+    credential_list = [] 
+
+    def __init__(self,accountName,username,password):
+
+        """
+        use __init__ method and define properties 
+        """
+        self.accountName = accountName
+        self.username = username
+        self.password = password
+
+    # def save_credential(self):
+
+    #     """
+    #     save_cridential method to saves users object into user list
+    #     """
+    #     Cridential.cridential_list.append(self)
