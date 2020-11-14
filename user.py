256c8bf5af9d7a956224eed683e6dc02c2cb74f2
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-14 13:19:08 +0200

    add delete method to the user

--- user.py
+++ user.py
@@ -18,3 +18,17 @@ class User:
         save_user method to saves users object into user list
         """
         User.user_list.append(self)
+
+    def delete_user(self):
+        """
+        delete user method to delete user object on the list
+        """
+      
+        User.user_list.remove(self)
+
+    # @classmethod
+    # def display_user(cls):
+    #     """
+    #     display user method to display list of user
+    #     """
+    #     return cls.user_list
