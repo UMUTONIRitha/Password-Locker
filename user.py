27561e0771016126ae1b9981a0c2dc4886952e41
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-14 11:21:40 +0200

    add def for save users

--- user.py
+++ user.py
@@ -2,12 +2,19 @@ class User:
     """
     class that generates username and password of users 
     """
-    user_list = [] #this will be used to insert list of username and password
+    user_list = [] 
 
-def __init__(self,username,password):
+    def __init__(self,username,password):
 
-    """
-    use __init__ method and define properties 
-    """
-    self.username = username
-    self.password = password
+        """
+        use __init__ method and define properties 
+        """
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+
+        """
+        save_user method to saves users object into user list
+        """
+        User.user_list.append(self)
