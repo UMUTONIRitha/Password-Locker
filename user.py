280d3a174d631b19d2759a3414bfe589135aefe2
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-14 10:36:02 +0200

    add class,list and username,password properties

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    class that generates username and password of users 
+    """
+    user_list = [] #this will be used to insert list of username and password
+
+def __init__(self,username,password):
+
+    """
+    use __init__ method and define properties 
+    """
+    self.username = username
+    self.password = password
